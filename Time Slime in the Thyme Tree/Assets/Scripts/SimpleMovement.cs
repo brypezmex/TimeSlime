5ed13b0c8a9a954e620140777833f6e26b3e1b1a
Bryan Perez <bryanp0138@gmail.com>
2024-11-13 13:33:04 -0500

    camera follows the slime

--- Time Slime in the Thyme Tree/Assets/Scripts/SimpleMovement.cs
+++ Time Slime in the Thyme Tree/Assets/Scripts/SimpleMovement.cs
@@ -49,7 +49,6 @@ public class SimpleMovement : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision) {
         if(collision.gameObject.tag == "Ground") 
-            anim.SetTrigger("landed");
             grounded = true;
     }
 }
